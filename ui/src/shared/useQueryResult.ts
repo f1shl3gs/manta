30c5b39c582840605de92e082adc3139b3b492c4
f1shl3gs <fishlegs.engerman@gmail.com>
2022-11-25 23:55:04 +0800

    remove transform functions

--- ui/src/shared/useQueryResult.ts
+++ ui/src/shared/useQueryResult.ts
@@ -1,10 +1,90 @@
 import {DashboardQuery} from 'src/types/Dashboard'
 import {useEffect, useState} from 'react'
 import {FromFluxResult, fromRows} from '@influxdata/giraffe'
-import {Row, transformToRows} from 'src/dashboards/transform'
 import {useAutoRefresh} from 'src/shared/useAutoRefresh'
 import {useParams} from 'react-router-dom'
 import {RemoteDataState} from '@influxdata/clockface'
+
+export type Result = {
+  metric: {
+    [key: string]: string
+  }
+  values: [[number, string]]
+}
+
+export type PromResp = {
+  status: string
+  data: {
+    resultType: string
+    result: Result[]
+  }
+}
+
+export interface Row {
+  [key: string]: string | number
+}
+
+export const transformToRows = (resp: PromResp): Row[] => {
+  if (!resp) {
+    return []
+  }
+
+  if (resp.status !== 'success') {
+    return []
+  }
+
+  return resp.data.result
+    .map((item: Result) => {
+      const {metric, values} = item
+
+      return values.map(val => {
+        return {
+          ...metric,
+          time: val[0] * 1000,
+          value: Number(val[1]),
+        }
+      })
+    })
+    .flat()
+}
+
+export const transformPromResp = (
+  resp?: PromResp
+): FromFluxResult | undefined => {
+  if (!resp) {
+    return undefined
+  }
+
+  if (resp.status !== 'success') {
+    return undefined
+  }
+
+  const rows = resp.data.result
+    .map((item: Result) => {
+      const {metric, values} = item
+
+      return values.map(val => {
+        return {
+          ...metric,
+          time: val[0] * 1000,
+          value: Number(val[1]),
+        }
+      })
+    })
+    .flat()
+
+  const table = fromRows(rows)
+  const groupKeys = table.columnKeys.filter(
+    key => key !== 'time' && key !== 'value'
+  )
+
+  return {
+    table,
+    fluxGroupKeyUnion: groupKeys,
+    // TODO:
+    resultColumnNames: [],
+  }
+}
 
 const useQueryResult = (queries: DashboardQuery[]) => {
   const [error, setError] = useState('')
