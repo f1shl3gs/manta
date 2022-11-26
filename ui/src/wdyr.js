55a04d11a77c2876528133968bba31e223771be0
f1shl3gs <fishlegs.engerman@gmail.com>
2022-11-27 00:31:27 +0800

    disable eslint warning

--- ui/src/wdyr.js
+++ ui/src/wdyr.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 if (process.env.NODE_ENV === 'development') {
-  // @ts-ignore
+  // eslint-disable-next-line
   const whyDidYouRender = require('@welldone-software/why-did-you-render')
   whyDidYouRender(React, {
     trackAllPureComponents: true,
