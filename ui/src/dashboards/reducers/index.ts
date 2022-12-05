89444fbb51691dbdc89dc2a969d65bb0a5f38594
f1shl3gs <fishlegs.engerman@gmail.com>
2022-12-06 01:21:39 +0800

    fix add cell test

--- ui/src/dashboards/reducers/index.ts
+++ ui/src/dashboards/reducers/index.ts
@@ -5,6 +5,12 @@ import {RemoteDataState} from '@influxdata/clockface'
 import {DEFAULT_DASHBOARD_SORT_OPTIONS} from 'src/constants/dashboard'
 
 import {
+  Action as CellAction,
+  SET_CELL,
+  SET_CELLS,
+} from 'src/cells/actions/creators'
+
+import {
   Action,
   EDIT_DASHBOARD,
   REMOVE_DASHBOARD,
@@ -34,7 +40,7 @@ const initialState = (): DashboardsState => ({
 
 export const dashboardsReducer = (
   state: DashboardsState = initialState(),
-  action: Action
+  action: Action | CellAction
 ): DashboardsState => {
   return produce(state, draftState => {
     switch (action.type) {
@@ -62,6 +68,35 @@ export const dashboardsReducer = (
       case SET_DASHBOARD_SEARCH_TERM:
         draftState.searchTerm = action.searchTerm
         return
+
+      case SET_CELL:
+        const {schema} = action
+
+        const cellID = schema.result
+        const cell = schema.entities.cells[cellID]
+        const dashboards = draftState.byID[cell.dashboardID]
+
+        if (dashboards?.cells.includes(cellID)) {
+          return
+        }
+
+        if (draftState.byID[cell.dashboardID]) {
+          draftState.byID[cell.dashboardID].cells.push(cellID)
+        }
+
+        return
+
+      case SET_CELLS: {
+        const {dashboardID, schema} = action
+        const cellIDs = schema && schema.result
+
+        if (!cellIDs) {
+          return
+        }
+
+        draftState.byID[dashboardID].cells = cellIDs
+        return
+      }
 
       default:
         return
