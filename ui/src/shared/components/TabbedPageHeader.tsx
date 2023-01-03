6d8d57a06d367c918520f08ccd8c62d11cf30dc7
f1shl3gs <fishlegs.engerman@gmail.com>
2023-01-03 15:26:54 +0800

    add style

--- ui/src/shared/components/TabbedPageHeader.tsx
+++ ui/src/shared/components/TabbedPageHeader.tsx
@@ -1,13 +1,14 @@
-import {FunctionComponent} from 'react'
+import React, {CSSProperties, FunctionComponent} from 'react'
 
 interface Props {
   left: JSX.Element | JSX.Element[]
   right: JSX.Element | JSX.Element[]
+  style?: CSSProperties
 }
 
-const TabbedPageHeader: FunctionComponent<Props> = ({left, right}) => {
+const TabbedPageHeader: FunctionComponent<Props> = ({left, right, style}) => {
   return (
-    <div className={'tabbed-page--header'}>
+    <div className={'tabbed-page--header'} style={style}>
       <div className={'tabbed-page--header-left'}>{left}</div>
 
       <div className={'tabbed-page--header-right'}>{right}</div>
